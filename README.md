318b61467fc2b685358aa84e629491375b21e549
kevinnadar <jesikamaraj@gmail.com>
2023-03-19 00:51:00 +0530

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Blog-GPT
-Blog-GPT is a Github repository that provides a simple and efficient solution for generating high-quality blog content using the OpenAI GPT-3.5 API. With this repository, bloggers and content creators can easily generate engaging and informative blog posts on a variety of topics without spending hours brainstorming and writing.
+Blog Post Generator using OpenAI GPT-3.5
+========================================
+
+This is a web application that generates blog posts using OpenAI GPT-3.5 and is built using Streamlit. With this application, users can generate unique blog posts on any topic by simply entering a title and a few keywords.
+
+Description
+-----------
+
+The Blog Post Generator using OpenAI GPT-3.5 is a web application that makes use of OpenAI's cutting-edge language generation technology to generate unique blog posts on any topic. The user simply inputs a title and a few related keywords, and the application generates a unique blog post based on the input.
+
+Usage
+-----
+
+To use this application, you can follow the instructions below:
+
+1.  Clone the repository from [https://github.com/kevinnadar22/Blog-GPT](https://github.com/kevinnadar22/Blog-GPT)
+2.  Install the required dependencies using the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+3.  Set your OpenAI API key as an environment variable using the following command:
+
+
+```bash
+export OPENAI_API_KEY=<your-api-key>
+```
+
+4.  Run the Streamlit app using the following command:
+
+
+```bash
+streamlit run main.py
+```
+
+
+5.  Open the URL displayed in your terminal to access the web application
+6.  Enter a title and a few keywords related to your blog post topic
+7.  Click on "Generate Blog Post" and wait for the AI to generate a blog post for you
+8.  You can download the generated blog post as a text file by clicking the "Download" button.
+
+Obtaining API Key
+-----------------
+
+In order to use the OpenAI GPT-3.5 API, you will need to obtain an API key. You can do this by following these steps:
+
+1.  Go to [https://beta.openai.com/signup/](https://beta.openai.com/signup/)
+2.  Create an account and follow the instructions to get started
+3.  Once you have access to the API, copy your API key
+
+Credits
+-------
+
+This application was built using Streamlit and the OpenAI GPT-3.5 API. Special thanks to the Streamlit team for providing an easy-to-use platform for building web applications.
+
+Author: Kevin Nadar GitHub: [https://github.com/kevinnadar22](https://github.com/kevinnadar22)
---
+++ main.py
@@ -0,0 +1,127 @@
+# Description: Streamlit app to generate blog posts using OpenAI GPT-3
+# Author: Kevin Nadar
+
+__title__ = "Blog GPT"
+__author__ = "Kevin Nadar"
+
+
+import os
+import random
+import string
+
+import hydralit_components as hc
+import openai
+import streamlit as st
+
+# Authenticate with OpenAI API using your API key
+# (you can find your API key at https://openai.com/)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Define function to generate blog post
+menu_items = {
+    'Get Help': 'https://telegram.me/ask_admin001',
+    'Report a bug': "https://github.com/kevinnadar22/Blog-GPT/issues",
+    'About': "Blog-GPT is a Github repository that generates high-quality blog content using the OpenAI GPT-3.5 API. With a simple prompt, users can quickly create engaging and informative posts without sacrificing quality. Ideal for bloggers and content creators looking to enhance their content creation process."
+}
+
+
+def generate_blog_post(title, body_snippet=None):
+    # Set up the prompt for OpenAI
+    prompt = f"Write a blog post titled '{title}'"
+    if body_snippet:
+        prompt += f" with the following body snippet: '{body_snippet}'"
+    prompt += "."
+    prompt += "If this doesn't seems like a blog post title or body snippet, simply reply with exactly 'Error'"
+    prompt += "The post should be at least 300 words long"
+
+    # Call OpenAI API to generate blog post
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+    )
+    # Extract the generated text from the response
+    generated_text = response.choices[0].message.content
+
+    return generated_text
+
+
+def download_button(generated_text):
+    st.write("Click the button below to download the generated text.")
+    random_string = "".join(random.choices(
+        string.ascii_letters + string.digits, k=5))
+    filename = f"generated_blog_post_{random_string}.txt"
+    if st.download_button(
+        label="Download Generated Text",
+        data=generated_text,
+        file_name=filename,
+        mime="text/plain",
+        key="download_button",
+    ):
+
+        with open(filename, "w") as f:
+            f.write(generated_text)
+
+        # Delete the generated text file after the download is finished
+        if "download_button" in st.session_state and st.session_state.download_button:
+            os.remove(filename)
+            st.session_state.download_button = False
+
+
+def post_process_text(generated_text):
+    # Add a "Save to File" button to save the generated text to a file
+    st.write("")
+    download_button(generated_text)
+
+
+# Define Streamlit app
+
+
+def app():
+
+    # Set app title and description
+    st.set_page_config(page_title=__title__,
+                       page_icon=":memo:", layout="centered")
+    st.title(__title__)
+    st.write("Enter the details below to generate a blog post using OpenAI.")
+
+    title_placeholder = "Bitcoin is the future"
+    body_snippet_placeholder = "Bitcoin is the future of money. It is the future of the world. It is the future of the universe. It is the future of everything."
+
+    title = st.text_input("Title", placeholder=title_placeholder)
+    body_snippet = st.text_area(
+        "Body Snippet", placeholder=body_snippet_placeholder)
+
+    # Generate blog post and display to user
+    if st.button("Clear", key="clear_results"):
+        st.empty()
+
+    if st.button("Generate Blog Post"):
+        if title == "":
+            st.error("Please enter a title for your blog post.", icon="❌")
+        else:
+            st.empty()
+            # with st.spinner("Generating blog post..."):
+            with hc.HyLoader('<h4>This may take a few minutes<h4>', hc.Loaders.standard_loaders, index=3, height=100):
+                try:
+                    generated_text = generate_blog_post(title, body_snippet)
+                except Exception as e:
+                    print(e)
+                    st.error(
+                        "An error occurred while generating the blog post. Please try again later.")
+                    return
+
+                if "error" in generated_text.lower():
+                    st.error(
+                        "Trye again with a different proper title or body snippet.")
+                else:
+                    st.success("Blog post generated successfully!")
+                    # Define the content to be displayed
+                    st.markdown(f"## {title}", unsafe_allow_html=True)
+                    st.write(generated_text)
+                    post_process_text(generated_text)
+                    st.balloons()
+
+
+# Run the app
+if __name__ == "__main__":
+    app()
